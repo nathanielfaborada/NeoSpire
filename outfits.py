c6c2a2f73bdc9b63764d166d67757358b0f34017
nathanielfaborada <faboradanathaniel@gmail.com>
2025-12-02 09:00:45 +0800

    DEVELOPMENT

--- outfits.py
+++ outfits.py
@@ -1,20 +1,18 @@
 import random
+from highrise.models import Item
 
-SHIRTS = {
-    "shirt-n_starteritems2019tankwhite": {"item_name": "Tank - White"},
-    "shirt-n_starteritems2019tankblack": {"item_name": "Tank - Black"},
-    "shirt-n_starteritems2019raglanwhite": {"item_name": "Raglan - White"},
-    "shirt-n_starteritems2019raglanblack": {"item_name": "Raglan - Black"},
-    "shirt-n_starteritems2019pulloverwhite": {"item_name": "Pullover - White"},
-    "shirt-n_starteritems2019pulloverblack": {"item_name": "Pullover - Black"},
-    "shirt-n_room32019jerseywhite": {"item_name": "White Vintage Jersey"},
-    "shirt-n_room12019buttondownblack": {"item_name": "Black Button Down"},
-    "shirt-n_flashysuit": {"item_name": "Flashy Suit"},
-}
+SHIRTS = [
+    "shirt-n_starteritems2019tankwhite",
+    "shirt-n_starteritems2019tankblack",
+    "shirt-n_starteritems2019raglanwhite",
+    "shirt-n_starteritems2019raglanblack",
+    "shirt-n_starteritems2019pulloverwhite",
+    "shirt-n_starteritems2019pulloverblack",
+]
+
+
 def random_outfit():
-    """Return a full random outfit dictionary."""
-    outfit = {}
-
-    outfit.update(random.choice(list(SHIRTS.items()))[1])
-    
+    """Return a list of Highrise Item objects."""
+    outfit = [
+        Item(type="shirt", id=random.choice(SHIRTS))]
     return outfit
